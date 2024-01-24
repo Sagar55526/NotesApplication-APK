8f3af148ce0db01d3c2ccbb8c1b813179b753ce6
sagar <ahiresagar06130@gmail.com>
2024-01-24 14:49:57 +0530

    Add project files.

---
+++ NotesApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NotesApplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
